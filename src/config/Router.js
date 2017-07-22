19496c85b89a0790d9c036bb4ee865e7bfa7a3d2
marleysidapple <bsiddhartha25@gmail.com>
2017-07-22 09:57:08 +0800

    added tab navigator

--- src/config/Router.js
+++ src/config/Router.js
@@ -1,15 +1,35 @@
-import { StackNavigator} from 'react-navigation';
+import { StackNavigator, TabNavigator }  from 'react-navigation';
 import SecondScreen  from './../screens/SecondScreen';
 import HomeScreen  from './../screens/HomeScreen';
+import { Icon } from 'react-native-elements';
+
+
+const Tabs = TabNavigator({
+  Home: {
+    screen: HomeScreen,
+    // navigationOptions: {
+    //   tabBarLabel: 'Feed',
+    //   tabBarIcon: ({ tintColor }) => <Icon name="list" size={35} color={tintColor} />,
+    // },
+  },
+  SecondScreen: {
+    screen: SecondScreen,
+    // navigationOptions: {
+    //   tabBarLabel: 'Me',
+    //   tabBarIcon: ({ tintColor }) => <Icon name="account-circle" size={35} color={tintColor} />
+    // },
+  },
+});
 
 
 const SimpleApp = StackNavigator({
   Home: { screen: HomeScreen },
-  SecondScreen: {screen: SecondScreen, title: 'ss'},
+  SecondScreen: {screen: Tabs}
 },
 {
   initialRouteNmae: 'Home'
 });
+
 
 
 export default SimpleApp;
